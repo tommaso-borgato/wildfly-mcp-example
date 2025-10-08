f1b2ff2b530ed110ad4d2488ddf97bd0ed3aaf5e
Tommaso Borgato <tborgato@redhat.com>
2025-10-08 09:31:21 +0200

    foo

--- server/src/main/java/org/wildfly/examples/mcp/server/ExampleTool.java
+++ server/src/main/java/org/wildfly/examples/mcp/server/ExampleTool.java
@@ -2,12 +2,14 @@ package org.wildfly.examples.mcp.server;
 
 import org.wildfly.extension.mcp.injection.tool.McpTool;
 import org.wildfly.mcp.api.Tool;
+import org.wildfly.mcp.api.ToolArg;
 
 @McpTool
 public class ExampleTool {
 
 	@Tool(name = "String.length", description = "Calculates the length of a string")
-	int stringLength(String s) {
+	int stringLength(
+			@ToolArg(description = "The string of which you want to calculate the length")  String s) {
 		System.out.println("Called stringLength() with s='" + s + "'");
 		return s.length();
 	}
